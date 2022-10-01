de75752760d1a0dd6b4c52362e1e4ad238214ff5
aaryan2134 <aaryan2134@gmail.com>
2022-10-02 03:42:54 +0530

    Added NFT minting
    Using DeSo Blockchain

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import NFT from './Components/NFT';
+import { BrowserRouter as Router } from "react-router-dom";
+function App() {
+  return (
+    <Router>
+     
+       <NFT/>
+    </Router>
+
+  );
+}
+
+export default App;
